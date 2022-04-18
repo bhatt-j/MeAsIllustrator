043a96c438d9da6a99ec17a7aa20319995321113
Janvi Bhatt <jb.janvibhatt@gmail.com>
2022-04-18 16:15:26 +0530

    post review with login

--- measillustrator/server/app.js
+++ measillustrator/server/app.js
@@ -1,25 +1,41 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const DB = 'mongodb+srv://MeAsIllustrator:MeAsIllustrator@cluster0.6t8yd.mongodb.net/MeAsIllustrator?retryWrites=true&w=majority';
-mongoose.connect(DB
-    //     ,{
-    //     useNewUrlParser:true,
-    //     useCreateIndex:true,
-    //     useUnifiedTopology:true,
-    //     useFindAndModify:false
-    // }
-    ).then(()=>{
-        console.log("connection successful");
-    }).catch((err)=> console.log('no connection'));
-    
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-//MeAsIllustrator
-app.get('/',(req,res) => {
-    res.send(`Hello world`);
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(3000,() =>{
-    console.log(`Server is running`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
